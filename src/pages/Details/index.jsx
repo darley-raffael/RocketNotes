617b1c0e0974cea150330518e4dc25e0479a0369
--add <--add>
2022-11-10 11:43:48 -0300

    adicionando componentes: button

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,11 +1,15 @@
 import "./style.js"
 import { Container } from "./style.js"
+import { Button } from "../../components/button"
 
 export default function Details() {
   return(
     <Container>
       <h1>olá mundo</h1>
       <p>darley Raffael </p>
+      <Button title="Clique aqui" loading />
+      <Button title="Entrar" />
+      <Button title="Voltar" />
     </Container>
   )
 }
--- src/pages/Details/style.js
+++ src/pages/Details/style.js
@@ -1,7 +1,5 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    background-color: #123211;
-    
-
+   
 `;
