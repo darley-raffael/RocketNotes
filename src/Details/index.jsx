24b99b2e814a612ef7be4528229bc0bedea32a8d
Dárley Raffael <darleyraffael@gmail.com>
2022-11-10 08:39:15 -0300

    usando o styled-components

--- src/Details/index.jsx
+++ src/Details/index.jsx
@@ -1,10 +1,11 @@
-import "./style.css"
+import "./style.js"
+import { Container } from "./style.js"
 
 export default function Details() {
   return(
-    <>
+    <Container>
       <h1>olá mundo</h1>
       <p>darley Raffael </p>
-    </>
+    </Container>
   )
 }
