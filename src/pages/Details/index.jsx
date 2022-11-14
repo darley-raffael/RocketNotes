dd8ba321090dbae1ae6d59a200d6a5b2c5daec92
Dárley Raffael <darleyraffael@gmail.com>
2022-11-14 07:20:23 -0300

    componente tags

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -3,6 +3,7 @@ import { Container, Links } from "./style.js"
 import { Header } from "../../components/header/index.jsx"
 import { Button } from "../../components/button"
 import { Section } from "../../components/section"
+import { Tag } from "../../components/tags"
 
 export default function Details() {
   return (
@@ -15,6 +16,11 @@ export default function Details() {
           <li><a href="#">https://www.rocketseat.com.br</a></li>
           <li><a href="#">https://www.rocketseat.com.br</a></li>
         </Links>
+      </Section>
+
+      <Section title="Marcadores" >
+          <Tag title="Express" />
+          <Tag title="Node" />
       </Section>
 
       <Button title="Voltar" />
