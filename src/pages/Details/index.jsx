1a861626224a497b62faf1dbec50f2ed0334bf39
Dárley Raffael <darleyraffael@gmail.com>
2022-11-21 06:58:08 -0300

    finalizandos componentes da página de notas

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,6 +1,7 @@
 import "./style.js"
 import { Container, Links } from "./style.js"
 import { Header } from "../../components/header/index.jsx"
+import { Content } from "./style"
 import { Button } from "../../components/button"
 import { Section } from "../../components/section"
 import { Tag } from "../../components/tags"
@@ -11,22 +12,31 @@ export default function Details() {
 
     <Container>
       <Header />
+      <main>
+        <Content>
 
-    <ButtonText title="Excluir nota" />
+          <ButtonText title="Excluir nota" />
+          <h1>Introdução ao React</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam accusantium tenetur repudiandae nihil quod facere earum ad at possimus corrupti, excepturi fuga! Delectus deserunt numquam non unde obcaecati sequi qui! 
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum porro, voluptatum temporibus expedita quisquam, ducimus quasi molestias rem repellat architecto velit soluta! Natus nemo, ullam hic labore enim beatae magnam!
+          </p>
+          <Section title="Links Úteis" >
+            <Links>
+              <li><a href="#">https://www.rocketseat.com.br</a></li>
+              <li><a href="#">https://www.rocketseat.com.br</a></li>
+            </Links>
+          </Section>
 
-      <Section title="Links Úteis" >
-        <Links>
-          <li><a href="#">https://www.rocketseat.com.br</a></li>
-          <li><a href="#">https://www.rocketseat.com.br</a></li>
-        </Links>
-      </Section>
+          <Section title="Marcadores" >
+            <Tag title="Express" />
+            <Tag title="Node" />
+          </Section>
 
-      <Section title="Marcadores" >
-          <Tag title="Express" />
-          <Tag title="Node" />
-      </Section>
+          <Button title="Voltar" />
+        </Content>
+      </main>
 
-      <Button title="Voltar" />
     </Container>
   )
 }
