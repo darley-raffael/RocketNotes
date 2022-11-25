09b48626427436bd174d88cda6f0fa970c849b28
Dárley Raffael <darleyraffael@gmail.com>
2022-11-25 07:38:10 -0300

    iniciando criação de page SigIn - outros ajustes

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
 import GlobalStyles from './styles/global'
-import { Home } from './pages/Home'
+import { SigIn } from './pages/SigIn'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <Home />
+      <SigIn />
     </ThemeProvider>
   </React.StrictMode>
 )
