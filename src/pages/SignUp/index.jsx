77d01b5b163e388f09424a8b7945ee05ad3cd541
Dárley Raffael <darleyraffael@gmail.com>
2022-11-28 07:20:11 -0300

    criando a pagina de SingnUp

---
+++ src/pages/SignUp/index.jsx
@@ -0,0 +1,43 @@
+import { Container, Form, Background } from "./styles";
+import { Input } from '../../components/input'
+import { FiMail, FiLock, FiUser} from 'react-icons/fi'
+import { Button } from '../../components/button'
+
+
+
+export function SignUp() {
+    return (
+        <Container>
+                <Background />
+            <Form>
+                <h1>Rocket Notes</h1>
+                <p>Aplicação para salvar e gerenciar seus links úteis.</p>
+
+                <h2>Crie sua conta</h2>
+                <Input
+                    placeholder="Nome"
+                    type="text"
+                    icon={FiUser}
+                />
+                <Input
+                    placeholder="E-mail"
+                    type="text"
+                    icon={FiMail}
+                />
+
+                <Input
+                    placeholder="Senha"
+                    type="password"
+                    icon={FiLock}
+                />
+
+
+                <Button title="Cadastrar" />
+
+                <a href="#">Voltar ao Login</a>
+
+            </Form>
+
+        </Container>
+    )
+}
