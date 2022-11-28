7655ce5ac85a32735fc3608cddb9b2b06f7b11a8
--add <--add>
2022-11-28 16:12:06 -0300

    componente: noteItem, finalizando a pagina New

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,10 +1,14 @@
 import { Header } from '../../components/header'
 import { Input } from '../../components/input'
-import { Textarea } from '../../components/Textarea'
+import { Textarea } from '../../components/textarea';
+import { NoteItem } from '../../components/noteItem';
+import { Section } from '../../components/section';
+import { Button } from '../../components/Button';
+
 import { Container, Form } from "./styles";
 
 
-export function New(){
+export function New() {
     return (
         <Container>
             <Header />
@@ -17,6 +21,21 @@ export function New(){
                     </header>
                     <Input placeholder="Título" />
                     <Textarea placeholder="Observações" />
+
+                    <Section title="Links Úteis" >
+                        <NoteItem value="https://www.rocketseat.com.br/" />
+
+                        <NoteItem isNew placeholder="Novo link" />
+                    </Section>
+
+                    <Section title="Marcadores">
+                        <div className="tags" >
+                            <NoteItem value="React" />
+                            <NoteItem isNew placeholder="Nova tag" />
+                        </div>
+                    </Section>
+
+                    <Button title="Salvar" />
                 </Form>
             </main>
         </Container>
