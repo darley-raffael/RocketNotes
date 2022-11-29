da5a8f203062fe16ae97c44a870f9ca2458351cb
--add <--add>
2022-11-29 09:31:21 -0300

    finalizando as rotas

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -6,3 +6,13 @@ import { Home } from '../pages/Home';
 import { Details } from '../pages/Details';
 import { Profile } from '../pages/Profile';
 
+export function AppRoutes() {
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/new" element={<New />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/details/:id" element={<Details />} />
+        </Routes>
+    )
+}
