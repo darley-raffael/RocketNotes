cb078c392df6b4cbbad2a3ed58b6d928f3ffce3f
Dárley Raffael <darleyraffael@gmail.com>
2022-11-29 08:53:50 -0300

    Criando as rotas do app

---
+++ src/routes/app.routes.jsx
@@ -0,0 +1,8 @@
+import { Routes, Route } from "react-router-dom";
+
+// Importando os componentes das Rotas
+import { New } from '../pages/New';
+import { Home } from '../pages/Home';
+import { Details } from '../pages/Details';
+import { Profile } from '../pages/Profile';
+
