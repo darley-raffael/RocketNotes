d45679007fa111b77335d91d5fcc5cecbc93e5f5
Raffael.Sicoob <darley.farias@segurosicoob.com.br>
2023-02-15 12:44:13 -0300

    integração backend com o front, criando a regra de criação de usuário no SingIn

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,12 +1,42 @@
+import { useState } from "react";
 import { Container, Form, Background } from "./styles";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Input } from '../../components/input'
 import { FiMail, FiLock, FiUser} from 'react-icons/fi'
 import { Button } from '../../components/button'
 
+import { api } from "../../services/api";
+
+
 
 
 export function SignUp() {
+    const [ name, setName ] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const navigate = useNavigate();
+    
+    function handleSignUp(){
+        if (!name || !email || !password) {
+            return alert("Preencha todos os campos corretamente!")                  
+        }
+
+        api.post("/users", { name, email, password })
+        .then(() => {
+            alert("Usuário Cadastrado com Sucesso");
+            navigate("/")
+
+        })
+        .catch( error => {
+            if(error.response){
+                alert(error.response.data.message)
+            }else{
+                alert("Não foi possível cadastrar")
+            }
+        })
+    }
+
     return (
         <Container>
                 <Background />
@@ -19,21 +49,24 @@ export function SignUp() {
                     placeholder="Nome"
                     type="text"
                     icon={FiUser}
+                    onChange={ e => setName(e.target.value)}
                 />
                 <Input
                     placeholder="E-mail"
                     type="text"
                     icon={FiMail}
+                    onChange={ e => setEmail(e.target.value)}
                 />
 
                 <Input
                     placeholder="Senha"
                     type="password"
                     icon={FiLock}
+                    onChange={ e => setPassword(e.target.value)}
                 />
 
 
-                <Button title="Cadastrar" />
+                <Button title="Cadastrar" onClick={ handleSignUp } />
 
                 <Link to="/" >Voltar ao Login</Link>
 
