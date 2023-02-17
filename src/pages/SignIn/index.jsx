502e500188f0758508fe61299812f81e6ff87d89
Dárley Raffael <darleyraffael@gmail.com>
2023-02-16 23:12:03 -0300

    salvando os estados de altenticação de usuário

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Container, Form, Background } from "./styles";
 import { Link } from "react-router-dom";
 import { Input } from "../../components/input";
@@ -6,12 +7,20 @@ import { Button } from "../../components/button";
 
 import { useAuth } from "../../hooks/auth";
 
+ 
 
 
 
 export function SignIn() {
-	const data = useAuth();
-	console.log("MEU CONTEXTO => ", data);
+	const  [ email, setEmail] = useState("");
+	const [ password, setPassword ]  = useState("");
+	const { signIn } = useAuth();
+
+	function handleSignIn(){
+		signIn({ email, password });
+	}
+
+	
 	return (
 		<Container>
 			<Form>
@@ -23,16 +32,18 @@ export function SignIn() {
 					placeholder="E-mail"
 					type="text"
 					icon={FiMail}
+					onChange={ e => setEmail(e.target.value)}
 				/>
 
 				<Input
 					placeholder="Senha"
 					type="password"
 					icon={FiLock}
+					onChange={e => setPassword(e.target.value)}
 				/>
 
 
-				<Button title="Entrar" />
+				<Button title="Entrar"  onClick={handleSignIn} />
 
 				<Link to="/register">Criar conta</Link>
 
