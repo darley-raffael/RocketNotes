5bb35b097fdd72bc33a86036bfd9d48656675824
Raffael.Sicoob <darley.farias@segurosicoob.com.br>
2023-02-25 13:51:47 -0300

    eslint alterração

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,39 +1,71 @@
-import { Container, Brand, Menu, Search, Content, NewNote} from "./styles";
+import { useState, useEffect } from "react";
+import { Container, Brand, Menu, Search, Content, NewNote } from "./styles";
 import { Header } from "../../components/header";
-import { ButtonText } from "../../components/buttonText"; 
-import { FiPlus, FiSearch} from "react-icons/fi";
+import { ButtonText } from "../../components/buttonText";
+import { FiPlus, FiSearch } from "react-icons/fi";
 import { Input } from "../../components/input";
 import { Section } from "../../components/section";
 import { Note } from "../../components/note";
+import { api } from "../../services/api";
 
 
 
-export function Home(){
+export function Home() {
+	const [tags, setTags] = useState([]);
+	const [ tagSelected, setTagSelectad ] = useState([]);
+
+	function handleTagSelected(tagName){
+		setTagSelectad([tagName]);
+	}
+
+
+	useEffect(() => {
+		async function fetchTags() {
+			const response = await api.get("/tags");
+			setTags(response.data);
+		}
+
+		fetchTags();
+	}, []);
 	return (
 		<Container>
 			<Brand>
 				<h1>Rocketnotes</h1>
 			</Brand>
 
-			<Header/>
+			<Header />
 
 			<Menu>
-				<li><ButtonText title="Todos"/></li>
-				<li><ButtonText title="React"/></li>
-				<li><ButtonText title="Node"/></li>
+				<li>
+					<ButtonText
+						title="Todos"
+						isActive
+					/>
+				</li>
+				{
+					tags && tags.map(tag => (
+
+						<li>
+							<ButtonText
+								title={tag.name}
+							/>
+						</li>
+					))
+				}
+
 			</Menu>
 
 			<Search>
-				<Input placeholder="Pesquisar pelo título" icon={ FiSearch } />
+				<Input placeholder="Pesquisar pelo título" icon={FiSearch} />
 			</Search>
 
 			<Content>
 				<Section title="Minhas notas">
 					<Note data={{
-						title: "React", 
+						title: "React",
 						tags: [
-							{id: "1", name: "react"},
-							{id: "2", name: "node"}
+							{ id: "1", name: "react" },
+							{ id: "2", name: "node" }
 						]
 					}}
 					/>
@@ -42,8 +74,8 @@ export function Home(){
 			</Content>
 
 			<NewNote to="/new">
-				<FiPlus/>
-                Criar nota
+				<FiPlus />
+				Criar nota
 			</NewNote>
 		</Container>
 	);
