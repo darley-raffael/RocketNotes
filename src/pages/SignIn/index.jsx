b5386ba6ce4920a23cd97057773277885cf1bef2
Raffael.Sicoob <darley.farias@segurosicoob.com.br>
2023-02-26 12:33:16 -0300

    button import

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,7 +1,7 @@
 import { Container, Form, Background } from "./styles";
-import { FiMail, FiLock } from "react-icons/fi";
 import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
+import { FiMail, FiLock } from "react-icons/fi";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 
